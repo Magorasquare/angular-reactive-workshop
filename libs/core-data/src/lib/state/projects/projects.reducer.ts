5ab1e5f3021f067403c4e1f0cc8e98f933aee374
Santiago Gómez Grajales <santiago.gomez@unosquare.com>
2024-01-15 16:35:26 -0500

    [01-ngrx-store] Added reducer and initial states for 'projects' feature

--- libs/core-data/src/lib/state/projects/projects.reducer.ts
+++ libs/core-data/src/lib/state/projects/projects.reducer.ts
@@ -32,3 +32,23 @@ const updateProject = (projects, project) => projects.map(p => {
   return p.id === project.id ? Object.assign({}, project) : p;
 });
 const deleteProject = (projects, project) => projects.filter(w => project.id !== w.id);
+
+// 01 - Define the shape of the states
+export interface ProjectsState {
+  projets: Project[];
+  selectedProjectId: string | null;
+};
+ 
+// 02 - Define the initial state 
+export const initialState: ProjectsState = {
+  projets: initialProjects,
+  selectedProjectId: null
+};
+
+// 03 - Build the MOST simplest reducer
+export function projectsReducer(state = initialState, action): ProjectsState {
+  switch (action.type) {  
+    default:
+      return state;
+  }
+}
